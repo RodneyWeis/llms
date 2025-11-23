2cecb14fa1150dfc1f65d8e57f92b1dc7594624e
Rod Weis <rodweis@gmail.com>
2025-11-22 19:04:17 -0700

    Parse command line argument for prompt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -7,8 +8,15 @@ api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
 
 client = genai.Client(api_key=api_key)
+try:
+	prompt = sys.argv[1]
+except IndexError:
+	print('Error: No command line argument provided')
+	sys.exit(1)
+		
+print(prompt)
 
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
 print(response.text)
 print('Prompt tokens:', response.usage_metadata.prompt_token_count)
 print('Response tokens:', response.usage_metadata.candidates_token_count)
